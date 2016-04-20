a8380be223d9df307270c15b4c1a0fa5b40675e3
vince421 <vincent.loreau@gmail.com>
2016-04-20 21:32:22 +0100

    my first py

---
+++ hello.py
@@ -0,0 +1,10 @@
+print('hello')
+annee = int(input('l annee: '))
+print(annee)
+if annee % 4 !=0:
+    print('not bi')
+elif  annee % 400 == 0 and annee % 100 ==0 :
+    print('bissextile!')
+else : print('not bi')
+
+print('end')
